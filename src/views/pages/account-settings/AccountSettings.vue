eb2366f29943c570903b5185d54cb920bc534933
Truong Manh Nguyen <truongmanh0912@gmail.com>
2022-06-26 22:24:01 +0700

    Edit

--- src/views/pages/account-settings/AccountSettings.vue
+++ src/views/pages/account-settings/AccountSettings.vue
@@ -1,18 +1,9 @@
 <template>
   <v-card id="account-setting-card">
     <!-- tabs -->
-    <v-tabs
-      v-model="tab"
-      show-arrows
-    >
-      <v-tab
-        v-for="tab in tabs"
-        :key="tab.icon"
-      >
-        <v-icon
-          size="20"
-          class="me-3"
-        >
+    <v-tabs v-model="tab" show-arrows>
+      <v-tab v-for="tab in tabs" :key="tab.icon">
+        <v-icon size="20" class="me-3">
           {{ tab.icon }}
         </v-icon>
         <span>{{ tab.title }}</span>
@@ -65,20 +56,20 @@ export default {
     const accountSettingData = {
       account: {
         avatarImg: require('@/assets/images/avatars/1.png'),
-        username: 'johnDoe',
-        name: 'john Doe',
-        email: 'johnDoe@example.com',
+        username: 'admin',
+        name: 'Nguyen Manh Truong',
+        email: 'truongmanh0912@gmail.com',
         role: 'Admin',
         status: 'Active',
-        company: 'Google.inc',
+        company: 'Sotatek',
       },
       information: {
-        bio: 'The name’s John Deo. I am a tireless seeker of knowledge, occasional purveyor of wisdom and also, coincidentally, a graphic designer. Algolia helps businesses across industries quickly create relevant 😎, scaLabel 😀, and lightning 😍 fast search and discovery experiences.',
-        birthday: 'February 22, 1995',
-        phone: '954-006-0844',
-        website: 'https://themeselection.com/',
-        country: 'USA',
-        languages: ['English', 'Spanish'],
+        bio: 'Hi i am a fullstack developer',
+        birthday: 'December 09, 2000',
+        phone: '0705133876',
+        website: 'fb.com/n.n.trg',
+        country: 'Vietnam',
+        languages: ['English', 'Vietnamese'],
         gender: 'male',
       },
     }
